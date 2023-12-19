524882daa9f8b312b70e69df5263a66f71da2e6e
Oranges-sour <873516725@qq.com>
2023-12-19 22:11:04 +0800

    1

--- data_process.py
+++ data_process.py
@@ -2,21 +2,21 @@
 import os
 
 # 图片路径
-path = "data"
+path = "data1"
 # 目标文件夹路径
-path1 = "data"
+path1 = "data1"
 filelist = os.listdir(path)
 
 j = 0
 
 for i in filelist:
     # 判断该路径下的文件是否为图片
-    if i.endswith(".png"):
+    if i.endswith(".jpg"):
         # 打开图片
         src = os.path.join(os.path.abspath(path), i)
         # 重命名
         dst = os.path.join(
-            os.path.abspath(path1), "a" + format(str(j), "0>3s") + ".png"
+            os.path.abspath(path1), "b" + f"{j}" + ".jpg"
         )
         # 执行操作
         os.rename(src, dst)
