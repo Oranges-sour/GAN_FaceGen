3db37530830ed8c2e75ead29a3b510f03ddfe79c
Oranges-sour <873516725@qq.com>
2023-12-20 20:48:23 +0800

    1

--- network.py
+++ network.py
@@ -63,7 +63,7 @@ class GeneratorNet(nn.Module):
         x = torch.relu(self.conv1(x))
         x = torch.relu(self.conv2(x))
         x = torch.relu(self.conv3(x))
-        x = torch.tanh(self.conv4(x))
+        x = torch.relu(self.conv4(x))
         x = torch.tanh(self.conv5(x))
 
         x = x.reshape(-1, 3, 64, 64)
@@ -83,18 +83,17 @@ class DiscriminatorNet(nn.Module):
         self.conv1 = nn.Conv2d(
             in_channels=3,
             out_channels=128,
-            kernel_size=3,
-            stride=1,
+            kernel_size=4,
+            stride=2,
             padding=1,
             device=device,
         )
-        self.pool1 = nn.MaxPool2d(2, 2)
 
         self.conv2 = nn.Conv2d(
             in_channels=128,
             out_channels=196,
-            kernel_size=3,
-            stride=1,
+            kernel_size=4,
+            stride=2,
             padding=1,
             device=device,
         )
@@ -102,31 +101,38 @@ class DiscriminatorNet(nn.Module):
         self.conv3 = nn.Conv2d(
             in_channels=196,
             out_channels=256,
-            kernel_size=3,
+            kernel_size=4,
             stride=2,
             padding=1,
             device=device,
         )
-        self.pool3 = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(256 * 8 * 8, 256, device=device)
-        self.fc2 = nn.Linear(256, 1, device=device)
+        self.conv4 = nn.Conv2d(
+            in_channels=256,
+            out_channels=256,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+            device=device,
+        )
+
+        self.conv5 = nn.Conv2d(
+            in_channels=256,
+            out_channels=1,
+            kernel_size=4,
+            stride=1,
+            padding=0,
+            device=device,
+        )
 
     def forward(self, x):
         x = x.reshape(-1, 3, 64, 64)
 
         x = self.leaky(self.conv1(x))
-        x = self.pool1(x)
         x = self.leaky(self.conv2(x))
         x = self.leaky(self.conv3(x))
-        x = self.pool3(x)
-
-        # print(x.shape)
-
-        x = x.reshape(-1, 256 * 8 * 8)
-
-        x = self.leaky(self.fc1(x))
-        x = self.fc2(x)
+        x = self.leaky(self.conv4(x))
+        x = self.conv5(x)
 
         x = x.reshape(-1, 1)
 
